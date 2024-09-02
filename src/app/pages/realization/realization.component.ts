c64a33558a6421d24a1e488484efa344da57a4b0
TomPrint <tomekklewicki@gmail.com>
2024-09-02 19:25:09 +0200

    create project structure and initialize navbar

---
+++ src/app/pages/realization/realization.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-realization',
+  standalone: true,
+  imports: [],
+  templateUrl: './realization.component.html',
+  styleUrl: './realization.component.css'
+})
+export class RealizationComponent {
+
+}
