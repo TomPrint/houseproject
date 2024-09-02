337fa237fa2016e285632f59555ae6d3198ab5a6
TomPrint <tomekklewicki@gmail.com>
2024-09-02 21:15:38 +0200

    add urls and navigation routes in navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,23 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FaqComponent } from './pages/faq/faq.component';
+import { RealizationComponent } from './pages/realization/realization.component';
+import { OfferComponent } from './pages/offer/offer.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
+import { IndividualComponent } from './pages/individual/individual.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, 
-  { path: '**', redirectTo: '', pathMatch: 'full' } 
+  { path: 'onas', component: AboutComponent },
+  {path: 'kontakt', component: ContactComponent},
+  {path: 'faq', component: FaqComponent},
+  {path: 'technologia', component: TechnologyComponent},
+  {path: 'oferta/catalog', component: CatalogComponent},
+  {path: 'oferta/realizacja', component:RealizationComponent},
+  {path: 'oferta/indywidualna', component: IndividualComponent},
+
+  { path: '**', redirectTo: '', pathMatch: 'full' }, // ! place this line always as last item !
 ];
