6d586844f1ea8dfce4f85f40d66dbf338246a2f8
Jakub Klewicki <kuba.klewicki@gmail.com>
2024-10-27 20:32:31 +0100

    O nas button works!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,13 +12,13 @@ import { IndividualComponent } from './pages/individual/individual.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent }, 
   { path: 'onas', component: AboutComponent },
-  {path: 'kontakt', component: ContactComponent},
-  {path: 'faq', component: FaqComponent},
-  {path: 'technologia', component: TechnologyComponent},
-  {path: 'oferta/spacer3D', component: OfferComponent},
-  {path: 'oferta/catalog', component: CatalogComponent},
-  {path: 'oferta/realizacja', component:RealizationComponent},
-  {path: 'oferta/indywidualna', component: IndividualComponent},
+  { path: 'kontakt', component: ContactComponent},
+  { path: 'faq', component: FaqComponent},
+  { path: 'technologia', component: TechnologyComponent},
+  { path: 'oferta/spacer3D', component: OfferComponent},
+  { path: 'oferta/catalog', component: CatalogComponent},
+  { path: 'oferta/realizacja', component:RealizationComponent},
+  { path: 'oferta/indywidualna', component: IndividualComponent},
 
   { path: '**', redirectTo: '', pathMatch: 'full' }, // ! place this line always as last item !
 ];
