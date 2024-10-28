a01bf3f2552cacc0c4052a818ace679e5855671e
Jakub Klewicki <kuba.klewicki@gmail.com>
2024-10-28 18:47:39 +0100

    Add splide section and footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import Splide from '@splidejs/splide';
+
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,16 @@ import { RouterModule } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    new Splide('#splide', {
+      type   : 'loop',
+      perPage: 1,
+      focus    : "center",
+      gap    : '1rem',
+      padding: '20%',
+      speed: 1800,
+      lazyLoad: 'nearby',
+    }).mount();
+  }
 }
