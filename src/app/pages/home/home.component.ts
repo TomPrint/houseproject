d43e205f6faf165e6ddb2516f4d68b462fb4395f
TomPrint <tomekklewicki@gmail.com>
2024-11-13 23:00:59 +0100

    move 3d walk to home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,25 +1,52 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, AfterViewInit, HostListener } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import Splide from '@splidejs/splide';
 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports:[RouterModule],
+  imports: [RouterModule, 
+            CommonModule, 
+            RouterLink, 
+            MatProgressSpinnerModule,
+            MatButtonModule,
+            MatTooltipModule,],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements AfterViewInit {
+  Walk3dSanitizedUrl: SafeResourceUrl;
+  loading = true; // Loading spinner
+
+  constructor(private sanitizer: DomSanitizer) {
+    const url = 'https://livetour.istaging.com/478b7e8f-43ca-4609-9391-1c8074e210fb?index=5';
+    this.Walk3dSanitizedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
   ngAfterViewInit() {
     new Splide('#splide', {
-      type   : 'loop',
+      type: 'loop',
       perPage: 1,
-      focus    : "center",
-      gap    : '1rem',
+      focus: 'center',
+      gap: '1rem',
       padding: '20%',
       speed: 1800,
       lazyLoad: 'nearby',
     }).mount();
+  }
+
+  
+
+  onIframeLoad() {
+    // Add a delay to make the spinner visible for at least 500ms
+    setTimeout(() => {
+      this.loading = false; // Hide the loading spinner after the delay
+    }, 500); // Adjust the delay duration as needed
   }
 }
--- src/app/pages/offer/offer.component.ts
+++ src/app/pages/offer/offer.component.ts
@@ -9,20 +9,20 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 @Component({
   selector: 'app-offer',
   standalone: true,
-  imports: [CommonModule, RouterLink, MatProgressSpinnerModule],
+  imports: [],
   templateUrl: './offer.component.html',
   styleUrls: ['./offer.component.css'] // Corrected here
 })
 export class OfferComponent {
-  Walk3dSanitizedUrl: SafeResourceUrl;
-  loading = true; // Loading spinner
+  // Walk3dSanitizedUrl: SafeResourceUrl;
+  // loading = true; // Loading spinner
 
-  constructor(private sanitizer: DomSanitizer) {
-    const url = 'https://livetour.istaging.com/478b7e8f-43ca-4609-9391-1c8074e210fb?index=5';
-    this.Walk3dSanitizedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
-  }
+  // constructor(private sanitizer: DomSanitizer) {
+  //   const url = 'https://livetour.istaging.com/478b7e8f-43ca-4609-9391-1c8074e210fb?index=5';
+  //   this.Walk3dSanitizedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  // }
 
-  onIframeLoad() {
-    this.loading = false; // Hide the loading spinner
-  }
+  // onIframeLoad() {
+  //   this.loading = false; // Hide the loading spinner
+  // }
 }
