231f366e2b770a2448f9ea2f719efe3593846981
TomPrint <tomekklewicki@gmail.com>
2024-12-14 11:23:56 +0100

    delete WYBIERZ button for interior/exterior change

--- src/app/partials/design/design.component.ts
+++ src/app/partials/design/design.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,51 +6,53 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './design.component.html',
-  styleUrl: './design.component.css'
+  styleUrls: ['./design.component.css']
 })
-export class DesignComponent implements OnChanges{
+export class DesignComponent implements OnInit, OnChanges {
   @Input() houseData: any; 
   @Input() selectedInterior: string = '';
   @Input() selectedExterior: string = '';
   @Input() currentInteriorIndex: number = 0; 
   @Input() currentExteriorIndex: number = 0;
 
-    // React to input changes
-    ngOnChanges() {
-      // Ensure indices are reset if new house data is provided
-      if (this.houseData) {
-        this.currentInteriorIndex = 0;
-        this.currentExteriorIndex = 0;
-   
-    // Clear previously selected interior and exterior
-    this.selectedInterior = '';
-    this.selectedExterior = '';
-
-        
-      }
-    }
-
-    nextInterior() {
-      this.currentInteriorIndex =
-        (this.currentInteriorIndex + 1) % this.houseData.interiors.length;
-    }
-  
-    // Navigate through exteriors
-    nextExterior() {
-      this.currentExteriorIndex =
-        (this.currentExteriorIndex + 1) % this.houseData.exteriors.length;
-    }
-  
-    // Select the current interior
-    selectInterior() {
-      this.selectedInterior = this.houseData.interiors[this.currentInteriorIndex].name;
-    }
-  
-    // Select the current exterior
-    selectExterior() {
-      this.selectedExterior = this.houseData.exteriors[this.currentExteriorIndex].name;
+  // Initialize with the first choices
+  ngOnInit() {
+    if (this.houseData) {
+      this.initializeSelections();
     }
   }
 
-  
+  // React to input changes (when houseData changes)
+  ngOnChanges() {
+    if (this.houseData) {
+      this.resetSelections();
+    }
+  }
 
+  // Initialize selections for the current house
+  private initializeSelections() {
+    this.selectedInterior = this.houseData.interiors[this.currentInteriorIndex].name;
+    this.selectedExterior = this.houseData.exteriors[this.currentExteriorIndex].name;
+  }
+
+  // Reset selections and indices when switching houses
+  private resetSelections() {
+    this.currentInteriorIndex = 0;
+    this.currentExteriorIndex = 0;
+    this.initializeSelections();
+  }
+
+  // Navigate to the next interior
+  nextInterior() {
+    this.currentInteriorIndex =
+      (this.currentInteriorIndex + 1) % this.houseData.interiors.length;
+    this.selectedInterior = this.houseData.interiors[this.currentInteriorIndex].name;
+  }
+
+  // Navigate to the next exterior
+  nextExterior() {
+    this.currentExteriorIndex =
+      (this.currentExteriorIndex + 1) % this.houseData.exteriors.length;
+    this.selectedExterior = this.houseData.exteriors[this.currentExteriorIndex].name;
+  }
+}
