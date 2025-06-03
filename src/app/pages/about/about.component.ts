3bc98fcb73a64a008e48aaab953fc9ae951aada6
TomPrint <tomekklewicki@gmail.com>
2025-06-03 21:19:14 +0200

    add house animation

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FooterComponent } from "../../shared/footer/footer.component";
 import { NavbarComponent } from "../../shared/navbar/navbar.component";
 import { RouterModule } from '@angular/router';
@@ -6,10 +7,25 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [FooterComponent, NavbarComponent, RouterModule],
+  imports: [FooterComponent, NavbarComponent, RouterModule, CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  animate = false;
+
+  triggerAnimation() {
+    this.animate = false;
+    setTimeout(() => {
+      this.animate = true;
+    }, 50); // Ensure DOM update
+  }
+
+  ngOnInit() {
+    // Optional: delay the start a little for smoother UX
+    setTimeout(() => {
+      this.animate = true;
+    }, 100);
+  }
 
 }
