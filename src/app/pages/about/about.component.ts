a04cf4fb9b9f63224bee56b3eacb60ca626c259d
TomPrint <tomekklewicki@gmail.com>
2025-06-03 20:34:13 +0200

    add about us component and privacy policy

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../shared/footer/footer.component";
+import { NavbarComponent } from "../../shared/navbar/navbar.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, NavbarComponent, RouterModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
